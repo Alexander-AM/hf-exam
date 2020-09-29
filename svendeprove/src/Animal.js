45c9ebda4b36517a34626f408f8c813152a28e12
4480135 <4480135@RTS-365.dk>
2020-09-29 15:19:07 +0200

    Animal page setup.

--- svendeprove/src/Animal.js
+++ svendeprove/src/Animal.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const Animal = () => {
-    return <main></main>;
-};
+const ENDPOINT = "https://dyrevelfaerd-alexander.herokuapp.com";
+
+class Animal extends React.Component {
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = { data: {} };
+    }
+
+    componentDidMount() {
+        fetch(`${ENDPOINT}/api/v1/animals/${1}`)
+            .then((e) => e.json())
+            .then((data) => {
+                this.setState({ data: data });
+            });
+    }
+
+    render() {
+        return <main>{0}</main>;
+    }
+}
 
 export default Animal;
