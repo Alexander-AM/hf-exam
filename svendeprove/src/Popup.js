5cde27231e1d71d3366b74484c9f243f4b1d0ab8
4480135 <4480135@RTS-365.dk>
2020-09-30 15:04:03 +0200

    Add popup.

---
+++ svendeprove/src/Popup.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Popup.css";
+
+const Popup = (props) => {
+    return (
+        <article
+            className="popup-container"
+            style={{ display: props.visible ? "flex" : "none" }}
+        >
+            <section className="popup">
+                <p className="popup-title">{props.title}</p>
+                <section className="popup-content">{props.children}</section>
+            </section>
+        </article>
+    );
+};
+
+export default Popup;
