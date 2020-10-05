765447d601b157a1f7ea34b3419ed9cf2ab10c78
4480135 <4480135@RTS-365.dk>
2020-10-05 10:25:21 +0200

    Moduralize colours.

--- svendeprove/src/index.js
+++ svendeprove/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./js/App";
 import Animal from "./js/Animal";
@@ -8,36 +8,41 @@ import Admin from "./js/Admin";
 import Header from "./js/Header";
 import Footer from "./js/Footer";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { AiOutlineLoading } from "react-icons/ai";
+
+import "./css/index.scss";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <Header />
+        <Suspense fallback={<AiOutlineLoading className="loading-icon" />}>
+            <Router>
+                <Header />
 
-            <Switch>
-                <Route path="/animal/:id">
-                    <Animal />
-                </Route>
+                <Switch>
+                    <Route path="/animal/:id">
+                        <Animal />
+                    </Route>
 
-                <Route path="/newsletter/:email">
-                    <Newsletter />
-                </Route>
+                    <Route path="/newsletter/:email">
+                        <Newsletter />
+                    </Route>
 
-                <Route path="/login">
-                    <Login />
-                </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
 
-                <Route path="/admin">
-                    <Admin />
-                </Route>
+                    <Route path="/admin">
+                        <Admin />
+                    </Route>
 
-                <Route path="/">
-                    <App />
-                </Route>
-            </Switch>
+                    <Route path="/">
+                        <App />
+                    </Route>
+                </Switch>
 
-            <Footer />
-        </Router>
+                <Footer />
+            </Router>
+        </Suspense>
     </React.StrictMode>,
     document.getElementById("root")
 );
